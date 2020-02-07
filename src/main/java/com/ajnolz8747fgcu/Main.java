704be27e5958d7fc9cb807d977f42fa3ff0ebb93
nolzaj93 <nolzaj93@gmail.com>
2020-02-06 19:53:45 -0500

    Refactored

--- src/main/java/com/ajnolz8747fgcu/Main.java
+++ src/main/java/com/ajnolz8747fgcu/Main.java
@@ -3,12 +3,17 @@ package com.ajnolz8747fgcu;
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.IOException;
 
+/**
+ * Main holds the main method, which is the starting point of the program.
+ *
+ * @author austinnolz This
+ *
+ */
 public class Main {
 
     public static void main(String[] args) throws IOException, CsvValidationException {
-	// write your code here
-      System.out.println("Hello.");
 
+      // Reads and prints the contents of SEOExample.csv
       CsvParser csvP = new CsvParser("src/Data/SEOExample.csv");
       csvP.printCsv();
     }
